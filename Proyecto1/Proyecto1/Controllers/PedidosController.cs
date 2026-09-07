54734cb49c4da66bc56f76503c181a91eae74466
YulFCh <yfch100@gmail.com>
2026-09-07 01:07:19 -0500

    A

---
+++ Proyecto1/Proyecto1/Controllers/PedidosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto1.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // ============================================================
+        // VISTA DE PEDIDOS
+        // GET: /Pedidos
+        // ============================================================
+
+        public IActionResult Index()
+        {
+            return View("Pedidos");
+        }
+    }
+}
+
---
+++ Proyecto1/Proyecto1/Views/Pedidos/Pedidos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto1.Views.Pedidos
+{
+    public class PedidosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
